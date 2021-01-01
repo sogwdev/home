f0956a78dbcf473e73d243137a4296f17ead458f
sogwdev <ogwdev01@gmail.com>
2021-01-01 22:42:03 +0900

    a

--- traveler/login.php
+++ traveler/login.php
@@ -10,7 +10,7 @@
 $name = $_POST["name"];
 $pwd = $_POST["pwd"];
 $db = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;port=3306;dbname=heroku_ef5f71273fe5d40;charset=utf8','b712f8e8641bf4','466f991d');
-$search = $db->query("select pwd where name=$name");
+$search = $db->query("select pwd from membership where name=$name");
 while($targetpwd = $search->fetch()){
 if ($pwd == $targetpwd) {
 header('Location: https://startpage001.herokuapp.com/traveler/success.php');
