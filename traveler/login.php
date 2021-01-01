dedd08eb0c98d97ad17dcbaf684c3d1b3e96ab14
sogwdev <ogwdev01@gmail.com>
2021-01-01 22:29:06 +0900

    a

--- traveler/login.php
+++ traveler/login.php
@@ -11,14 +11,13 @@ $name = $_POST["name"];
 $pwd = $_POST["pwd"];
 $db = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;port=3306;dbname=heroku_ef5f71273fe5d40;charset=utf8','b712f8e8641bf4','466f991d');
 $search = $db->query("select pwd where name=$name");
-$targetpwd = $search->fetch();
-
+while($targetpwd = $search->fetch()){
 if ($pwd == $targetpwd) {
 header('Location: https://startpage001.herokuapp.com/traveler/success.php');
 } else {
   print  "認証失敗";
 }
-
+}
  ?>
 </body>
 
