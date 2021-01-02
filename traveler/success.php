503937108c35ea2c0421fd3ce3401da931c1b797
sogwdev <ogwdev01@gmail.com>
2021-01-02 12:31:02 +0900

    a

--- traveler/success.php
+++ traveler/success.php
@@ -2,12 +2,18 @@
 <html>
 <head>
   <meta charset="UTF-8">
-
-
+  <link rel="stylesheet" href="assets/css/prefectly.css">
 </head>
 <body>
+  <ul class="prefectly" id="plain">
+    <li data-pref="ac" class="ac">A</li>
+    <li data-pref="am" class="am">B</li>
+    :
+    <li data-pref="ym" class="ym">t</li>
+    <li data-pref="yn" class="yn">u</li>
+</ul>
 
-<p>welcome</p>
+
 
 </body>
 
