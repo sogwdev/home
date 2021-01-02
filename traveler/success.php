9fcbc81b55a55aee5c7ae4b81cb789892b8c65fa
sogwdev <ogwdev01@gmail.com>
2021-01-02 14:36:09 +0900

    a

--- traveler/success.php
+++ traveler/success.php
@@ -1,39 +1,16 @@
-<!DOCTYPE HTML>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="assets/map.css">
+    <title>あなたの地図</title>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="assets/jquery/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="assets/jquery/jmap.js"></script>
+    <script type = "text/javascript">
+        $(document).ready(function() {
+            $('#jmap').jmap();
+        });
+    </script>
 </head>
 <body>
-  <!-- ●日本地図 -->
-		<ul id="imagemap">
-			<li id="hokkaido"><a href="hokkaido/hokkaido.html"><span>北海道</span></a></li>
-			<li id="touhoku"><a href="tohoku/miyagi</l"><span>東北</span></a></li>
-			<li id="hokuriku"><a href="hokuriku/nagano.html"><span>北陸</span></a></li>
-			<li id="kanto"><a href="kanto/tokyo.html"><span>関東</span></a></li>
-			<li id="tokai"><a href="toukai/shizuoka.html"><span>東海</span></a></li>
-			<li id="kansai"><a href="kansai/osaka.html"><span>関西</span></a></li>
-			<li id="chugoku"><a href="chugoku/hiroshima.html"><span>中国</span></a></li>
-			<li id="shikoku"><a href="shikoku/kochi.html"><span>四国</span></a></li>
-			<li id="kyushu"><a href="kyushu/fukuoka.html"><span>九州</span></a></li>
-			<li id="okinawa"><a href="okinawa/okinawa.html"><span>沖縄</span></a></li>
-		</ul>
-		<!-- ●日本地図下の地域リスト -->
-		<ul id="area">
-			<li><a href="hokkaido/hokkaido.html">北海道</a></li>
-			<li><a href="tohoku/miyagi.html">東北</a></li>
-			<li><a href="hokuriku/nagano.html">北陸・甲信越</a></li>
-			<li><a href="kanto/tokyo.html">関東</a></li>
-			<li><a href="toukai/shizuoka.html">東海</a></li>
-			<li><a href="kansai/osaka.html">関西</a></li>
-			<li><a href="chugoku/hiroshima.html">中国</a></li>
-			<li><a href="shikoku/kochi.html">四国</a></li>
-			<li><a href="kyushu/fukuoka.html">九州</a></li>
-			<li><a href="okinawa/okinawa.html">沖縄</a></li>
-		</ul>
-
-
-
+    <div id="jmap"></div>
 </body>
-
 </html>
